dbcb9533d8140a08491e640b746a8fe9a3c196fc
Drew Roen <drewroen@gmail.com>
2018-11-26 18:24:40 -0600

    Allows writing to a csv file. Final.csv shows data of every race of every runner currently in college in the IIAC

--- NewPRScraper.py
+++ NewPRScraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import re
 
 def getSchoolsFromLeague(leagueUrl, gender):
     tempResponse = requests.get(leagueUrl)
@@ -10,7 +12,7 @@ def getSchoolsFromLeague(leagueUrl, gender):
         temp = ''.join(t['href'].split())
         if "tfrrs.org/teams" in temp:
             if "college_" + gender + "_" in temp:
-                schoolLinks.append(temp[temp.index("tfrrs.org/teams") + 16:])
+                schoolLinks.append("http://" + temp[temp.index("tfrrs.org/teams"):])
     return schoolLinks
 
 def getAthletesFromSchool(schoolUrl):
@@ -21,13 +23,45 @@ def getAthletesFromSchool(schoolUrl):
     for t in allLinks:
         temp = ''.join(t['href'].split())
         if "tfrrs.org/athletes" in temp:
-            athleteLinks.append(temp[temp.index("tfrrs.org/athletes") + 19:])
+            athleteLinks.append("http://" + temp[temp.index("tfrrs.org/athletes"):])
     return athleteLinks
 
+def getDataFromAthlete(athleteUrl):
+    tempResponse = requests.get(athleteUrl)
+    tempContent = BeautifulSoup(tempResponse.content, "html.parser")
+    athleteName = tempContent.find('h3').text
+    allResults = tempContent.find('div', attrs={'id':'meet-results'})
+    athleteResults = []
+    dateRegex = re.compile("[a-zA-Z]{3} [0-9]{1,2}(-[0-9]{1,2})?, [0-9]{4}")
+    for race in allResults.find_all('div'):
+        if race.find('thead'):
+            meetInfo = ' '.join(race.find('thead').text.split())
+            meetName = meetInfo[:dateRegex.search(meetInfo).start()].rstrip()
+            meetDate = meetInfo[dateRegex.search(meetInfo).start():]
+            tempResult = [athleteName, meetName, meetDate]
+            i = 0
+            for info in race.find_all('td'):
+                if i == 3:
+                    athleteResults.append(tempResult)
+                    tempResult = [athleteName, meetName, meetDate]
+                    i = 0
+                tempResult.append(''.join(info.text.split()))
+                i = i + 1
+            athleteResults.append(tempResult)
+    return athleteResults
 
-print("Enter a conference or region URL.")
+print("Enter a league URL.")
 leagueUrl = input()
-for school in getSchoolsFromLeague(leagueUrl, 'm'):
-    for t in getAthletesFromSchool("http://tfrrs.org/teams/"+school):
-        print(t)
+finalPRList = []
+
+
+print("Writing...")
+with open("final.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=",", lineterminator='\n')
+    for school in getSchoolsFromLeague(leagueUrl, 'm'):
+        print(school)
+        for athlete in getAthletesFromSchool(school):
+            print(athlete)
+            csvWriter.writerows(getDataFromAthlete(athlete))
+finalPRList.sort(key=lambda x: x[1])
         
